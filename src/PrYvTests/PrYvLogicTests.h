388ebda6abe002b43d6b2be5a7fdf60f7e38f1e7
Victor Kristof <victor.kristof@me.com>
2013-02-19 11:20:26 +0100

    Unit Testing Enabled
    Added possibility of Logic Testing and Application Testing. Ready to
    code the tests methods for both logic and applicationg testing.

--- src/PrYvTests/PrYvLogicTests.h
+++ src/PrYvTests/PrYvLogicTests.h
@@ -2,12 +2,12 @@
 //  PrYvTests.h
 //  PrYvTests
 //
-//  Created by Victor Kristof on 21.01.13.
+//  Created by Victor Kristof on 19.02.13.
 //  Copyright (c) 2013 PrYv. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
 
-@interface PrYvTests : SenTestCase
+@interface PrYvLogicTests : SenTestCase
 
 @end
