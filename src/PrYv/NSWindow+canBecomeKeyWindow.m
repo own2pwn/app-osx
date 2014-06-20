b138c6d3fac7e0e31895f5e810f8bbf09e4b0080
Vicow <v.kristof@me.com>
2014-06-20 17:11:41 +0200

    Note text field configured, not working yet (BUG: when already logged, popover doesn't work well)

---
+++ src/PrYv/NSWindow+canBecomeKeyWindow.m
@@ -0,0 +1,13 @@
+//
+//  NSWindow+canBecomeKeyWindow.m
+//  Pryv
+//
+//  Created by Victor Kristof on 20.06.14.
+//  Copyright (c) 2014 Pryv. All rights reserved.
+//
+
+#import "NSWindow+canBecomeKeyWindow.h"
+
+@implementation NSWindow (canBecomeKeyWindow)
+
+@end
